9741a114c3ad5a2c3f03786497387eaa9aad6a79
ambiknds <ambikndsjobz@gmail.com>
2025-02-06 04:17:18 +0530

    update 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,7 +23,7 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
       <Header />
       {!showResults ? (
         <TypingTest onTestComplete={handleTestComplete} />
